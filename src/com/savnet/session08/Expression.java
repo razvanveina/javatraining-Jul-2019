c22f44bd3c096bb8904d8886514d1ce669b96c3e
Razvan <Razvan@DESKTOP-RV11EA4>
2019-08-27 20:43:51 +0300

    Session 08

---
+++ src/com/savnet/session08/Expression.java
@@ -0,0 +1,40 @@
+package com.savnet.session08;
+
+public class Expression {
+	public static void main(String[] args) {
+		System.out.println(evaluate("5+3*2-8/2"));
+		// System.out.println(evaluate("5+3-2"));
+		// exp(5+3-2) = exp(5) + exp(3-2) = exp(5) + exp(3) - exp(2)
+	}
+
+	private static int evaluate(String exp) {
+		int index = exp.indexOf('+');
+		if (index != -1) {
+			String left = exp.substring(0, index);
+			String right = exp.substring(index + 1);
+			return evaluate(left) + evaluate(right);
+		}
+
+		index = exp.indexOf('-');
+		if (index != -1) {
+			String left = exp.substring(0, index);
+			String right = exp.substring(index + 1);
+			return evaluate(left) - evaluate(right);
+		}
+
+		index = exp.indexOf('*');
+		if (index != -1) {
+			String left = exp.substring(0, index);
+			String right = exp.substring(index + 1);
+			return evaluate(left) * evaluate(right);
+		}
+
+		index = exp.indexOf('/');
+		if (index != -1) {
+			String left = exp.substring(0, index);
+			String right = exp.substring(index + 1);
+			return evaluate(left) / evaluate(right);
+		}
+		return Integer.parseInt(exp);
+	}
+}
